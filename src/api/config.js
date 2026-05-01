4d270f8b0ff29f710249d589c2122b538975798f
alamgirwebdesign <alamgirwebdesign@gmail.com>
2026-05-01 19:15:07 +0600

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 // src/api/config.js
 // ⚠️ আপনার cPanel domain দিন
-export const API_BASE = 'https://yourdomain.com/blood/api'
+export const API_BASE = 'https://blood.shohozvibe.com/blood/api'
 
 // উদাহরণ:
 // export const API_BASE = 'https://ulipurblood.com/api'
